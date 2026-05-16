637ec5b38ef56c3aa0199437123181fa6882d46d
Zonk1987 <s-haupt@hotmail.de>
2026-05-17 00:23:58 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -7,7 +7,7 @@ local _ENV = setmetatable({}, {
     end
 })
 
-local REPO_URL = "https://raw.githubusercontent.com/Zonk1987/CC-Tweaked-Programs/dev/"
+local REPO_URL = "https://raw.githubusercontent.com/Zonk1987/CC-Tweaked-Programs/main/"
 local MANIFEST_NAME = "manifest.lua"
 
 local args = { ... }
