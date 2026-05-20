146f8ad2cd03831c0e45f8cf9ea5fa0b15b60b4a
Zonk1987 <25836879+Zonk1987@users.noreply.github.com>
2026-05-20 13:31:37 +0000

    chore: auto-bump version and file sizes [skip ci]

--- Mekanism Portal Dialer Recall Sender/startup.lua
+++ Mekanism Portal Dialer Recall Sender/startup.lua
@@ -1,6 +1,6 @@
 --[[
 ================================================================================
-Mekanism Portal Recall Sender v1.0.070-main
+Mekanism Portal Recall Sender v1.0.107-main
 ================================================================================
 ]]
 --
@@ -223,7 +223,7 @@ function RecallSender:drawTerminalHeader()
 	term.clear()
 	term.setCursorPos(1, 1)
 	term.setTextColor(colors.cyan)
-	print("Mekanism Portal Recall Sender v1.0.070-main")
+	print("Mekanism Portal Recall Sender v1.0.107-main")
 	term.setTextColor(colors.gray)
 	local w, _ = term.getSize()
 	print(string.rep("-", w))
