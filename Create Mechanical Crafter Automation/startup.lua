45fa45870c97fc8857d1592447c8bdebe794ca70
Zonk1987 <25836879+Zonk1987@users.noreply.github.com>
2026-05-21 20:56:02 +0000

    chore: auto-bump version and file sizes [skip ci]

--- Create Mechanical Crafter Automation/startup.lua
+++ Create Mechanical Crafter Automation/startup.lua
@@ -1,6 +1,6 @@
 --[[
 ================================================================================
-Create Mechanical Crafter Automation v1.0.127-main
+Create Mechanical Crafter Automation v1.0.128-main
 ================================================================================
 Automates Mechanical Crafter grids from the 'Create' mod.
 Powered by Enterprise AppRuntime & Fiber Scheduler.
--- Mekanism Portal Dialer Recall Sender/startup.lua
+++ Mekanism Portal Dialer Recall Sender/startup.lua
@@ -1,6 +1,6 @@
 --[[
 ================================================================================
-Mekanism Portal Recall Sender v1.0.127-main
+Mekanism Portal Recall Sender v1.0.128-main
 ================================================================================
 Standardized Recall Sender for Interdimensional Portals.
 Powered by Enterprise AppRuntime & Fiber Scheduler.
