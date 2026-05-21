a76378c1f96b8516afb53bb47fe4f0dc5356f139
Zonk1987 <25836879+Zonk1987@users.noreply.github.com>
2026-05-21 10:07:29 +0000

    chore: auto-bump version and file sizes [skip ci]

--- Mekanism Portal Dialer Recall Sender/startup.lua
+++ Mekanism Portal Dialer Recall Sender/startup.lua
@@ -1,6 +1,6 @@
 --[[
 ================================================================================
-Mekanism Portal Recall Sender v1.0.110-main
+Mekanism Portal Recall Sender v1.0.113-main
 ================================================================================
 Standardized Recall Sender for Interdimensional Portals.
 Powered by Enterprise AppRuntime & Fiber Scheduler.
