03cf5fa9acf8eacc603fc4be2da7b467fa99f80d
Zonk1987 <25836879+Zonk1987@users.noreply.github.com>
2026-06-08 21:04:56 +0000

    chore: auto-bump version and file sizes [skip ci]

--- Mekanism Portal Dialer Hub/startup.lua
+++ Mekanism Portal Dialer Hub/startup.lua
@@ -1,6 +1,6 @@
 --[[
 ================================================================================
-Mekanism Portal Hub v1.0.136-main
+Mekanism Portal Hub v1.0.145-main
 ================================================================================
 Standardized Hub System for Interdimensional Portals.
 Powered by Enterprise AppRuntime & Fiber Scheduler.
